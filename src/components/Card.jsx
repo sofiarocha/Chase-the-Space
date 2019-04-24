b5692a1d5069e952cb01cbef8efb83a4a246fbcf
Sofia <sofiaestanqueirorocha@gmail.com>
2019-04-24 17:33:33 +0100

    create card component & add card component twice to home & change home strucutre

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <h1>ISS</h1>
+            <img src="https://images.unsplash.com/photo-1446776877081-d282a0f896e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" alt="iss" />
+            <p>The International Space Station is a large spacecraft in orbit around Earth. It serves as a home where crews of astronauts and cosmonauts live. The space station is also a unique science laboratory.</p>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import TitleHomeContainer from './TitleHomeContainer';
+import Card from './Card';
 
 const Home = () => {
     return (
-        <div>
-            <TitleHomeContainer />
+        <div className="container">
+            <div id="section1">
+                <TitleHomeContainer />
+            </div>
+            <div id="section2">
+                <Card />
+                <Card />
+            </div>
         </div>
     );
 };
