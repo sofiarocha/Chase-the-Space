8fcedf935878aeba98b9699fc172f3b0d28a3ffb
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-04-24 15:55:38 +0100

    start footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Footer from './Footer';
 
 const App = () => {
     return (
-        <div>
-            Hello World
-        </div>
+           <Footer />
     );
 };
 
