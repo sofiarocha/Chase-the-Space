e2c0f984884f1b027a202217f0b804feefd0653b
Sofia <sofiaestanqueirorocha@gmail.com>
2019-04-30 18:29:10 +0100

    implement fetch to obtain picture of the day

--- src/components/APOD.jsx
+++ src/components/APOD.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const APOD = () => {
-    return (
-        <div>
-            <h1>APOD</h1>
-        </div>
-    );
+class APOD extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pictureOfDay: "https://apod.nasa.gov/apod/image/1904/M33Meteor_Chokshi_960.jpg"
+        };
+    }
+
+    componentDidMount = () => {
+        this.onShowPictureOfDay();
+    }
+
+    onShowPictureOfDay = () => {
+        const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=DV4ZLxIJ4QeI9eIXsHYlutwXWI8SwPNwRkbagwWt";
+        fetch(apodUrl)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    render () {
+        const podStyle = {
+            backgroundImage: `url(${this.state.pictureOfDay})`
+        };
+        return (
+            <div>
+                <div className="pod" style={podStyle} />
+            </div>
+        );
+    }
 };
 
 export default APOD;
