56d89460190da8bbbae430e42c81f93f13aa7a85
Sofia <sofiaestanqueirorocha@gmail.com>
2019-04-30 16:54:05 +0100

    add link to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Card = ({ card }) => {
     return (
-        <div className="col-5">
+        <Link to={`/${card.title.toLowerCase()}`} className="col-4">
             <h1>{card.title}</h1>
             <img className="card-image" src={card.imageSrc} alt={card.title.toLowerCase()} />
             <p>{card.description}</p>
-        </div>
+        </Link>
     );
 };
 
