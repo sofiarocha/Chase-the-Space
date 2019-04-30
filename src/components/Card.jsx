034c1f388aad8fadf4cb56db842220e0502d9497
Sofia <sofiaestanqueirorocha@gmail.com>
2019-04-30 16:07:06 +0100

    add props to card component throw home component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ card }) => {
     return (
         <div className="col-5">
-            <h1>ISS</h1>
-            <img className="card-image" src="https://images.unsplash.com/photo-1446776877081-d282a0f896e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80" alt="iss" />
-            <p>The International Space Station is a large spacecraft in orbit around Earth. It serves as a home where crews of astronauts and cosmonauts live. The space station is also a unique science laboratory.</p>
+            <h1>{card.title}</h1>
+            <img className="card-image" src={card.imageSrc} alt={card.title.toLowerCase()} />
+            <p>{card.description}</p>
         </div>
     );
 };
