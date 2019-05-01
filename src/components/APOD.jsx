3ab0c752021d7fd313e2f1581dc700e2c27b5061
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-01 15:00:54 +0100

    add backgound image, fetch API and show/hide desciption

--- src/components/APOD.jsx
+++ src/components/APOD.jsx
@@ -4,7 +4,8 @@ class APOD extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pictureOfDay: "https://apod.nasa.gov/apod/image/1904/M33Meteor_Chokshi_960.jpg"
+            pictureOfDay: {},
+            moreInfo: false
         };
     }
 
@@ -16,19 +17,40 @@ class APOD extends Component {
         const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=DV4ZLxIJ4QeI9eIXsHYlutwXWI8SwPNwRkbagwWt";
         fetch(apodUrl)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then((data) => {
+                console.log(data);
+                this.setState({
+                    pictureOfDay: data
+                });
+            });
+    }
+
+    handleClick = () => {
+        this.setState((state) => {
+            return { moreInfo: !state.moreInfo };
+        });
     }
 
     render () {
+        const { pictureOfDay, moreInfo } = this.state;
         const podStyle = {
-            backgroundImage: `url(${this.state.pictureOfDay})`
+            backgroundImage: `url(${pictureOfDay.url})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100%"
         };
         return (
-            <div>
-                <div className="pod" style={podStyle} />
+            <div className="container">
+                <div className="col-9">
+                    <div className="pod" style={podStyle}>
+                        <button onClick={this.handleClick} type="button">More info</button>
+                        {moreInfo
+                        && <p><small>{pictureOfDay.explanation}</small></p>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
-};
+}
 
 export default APOD;
