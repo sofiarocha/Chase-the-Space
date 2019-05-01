07d7e030ea905fa951bda96a9b17b5251b6ff0ae
Isabella <isabellaafry@gmail.com>
2019-05-01 18:01:44 +0100

    find our lat and long

--- src/components/OverMe.jsx
+++ src/components/OverMe.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+
+
 const OverMe = () => {
+    const handleCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+        })
+    }
     return (
         <div className="container">
             <h1>ISS</h1>
             <p>The International Space Station will be over me in 5 minutes</p>
-            <button>Share</button>
+            <button onClick={handleCurrentLocation()}>Share</button>
         </div>
     );
 };
