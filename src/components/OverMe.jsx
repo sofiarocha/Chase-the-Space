a932aea2f3c025d72b619634712322c740eb1fce
Isabella <isabellaafry@gmail.com>
2019-05-02 15:03:42 +0100

    overme

--- src/components/OverMe.jsx
+++ src/components/OverMe.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-
-const OverMe = () => {
-    const handleCurrentLocation = () => {
+class OverMe extends Component {
+    position = () => {
         navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position);
-        })
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            
+            fetch(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`)
+                .then(response => response.json()) 
+                .then(data => {
+                    console.log(data);
+                });
+        });
     }
-    return (
-        <div className="container">
-            <h1>ISS</h1>
-            <p>The International Space Station will be over me in 5 minutes</p>
-            <button onClick={handleCurrentLocation()}>Share</button>
-        </div>
-    );
+        render(){
+            return (
+                <div className="container">
+                    <h5>The International Space Station will be over me in 5 minutes</h5>
+                    <div class="fb-share-button" data-href="https://sofiarocha.github.io/iss" data-layout="box_count" data-size="small">
+                        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsofiarocha.github.io%2FChase-the-Space%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+                    </div>
+                </div>
+            );
+        }
 };
 
 export default OverMe;
