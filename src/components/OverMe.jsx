10b20b24711a7db4b89fd73176ad4b542e746886
Isabella <isabellaafry@gmail.com>
2019-05-02 15:58:28 +0100

    time over me

--- src/components/OverMe.jsx
+++ src/components/OverMe.jsx
@@ -2,24 +2,42 @@ import React, { Component } from 'react';
 
 
 class OverMe extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            passtime: 0
+        };
+    }
+
+    componentDidMount = () => {
+        this.position();
+    }
     position = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
+            console.log(lat);
             
-            fetch(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`)
+            const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+            const fetchUrl = `http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`;
+            fetch(proxyUrl + fetchUrl)
                 .then(response => response.json()) 
                 .then(data => {
-                    console.log(data);
+                    console.log(data.response[0].risetime);
+                    this.setState ({
+                        passtime: data.response[0].risetime
+                    });
                 });
+                let newDate = new Date();
+                newDate.setTime(this.state.passtime*1000);
         });
     }
         render(){
             return (
                 <div className="container">
-                    <h5>The International Space Station will be over me in 5 minutes</h5>
-                    <div class="fb-share-button" data-href="https://sofiarocha.github.io/iss" data-layout="box_count" data-size="small">
-                        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsofiarocha.github.io%2FChase-the-Space%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+                    <h5>{`The International Space Station will be over me in ${this.state.passtime} minutes`}</h5>
+                    <div className="fb-share-button" data-href="https://sofiarocha.github.io/iss" data-layout="box_count" data-size="small">
+                        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsofiarocha.github.io%2FChase-the-Space%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
                     </div>
                 </div>
             );
