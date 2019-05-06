adb8402607b5cb48c5889bcfb3dfa4d5dd2528e5
Isabella <isabellaafry@gmail.com>
2019-05-06 17:23:18 +0100

    overhead timer

--- src/components/OverMe.jsx
+++ src/components/OverMe.jsx
@@ -10,32 +10,38 @@ class OverMe extends Component {
     }
 
     componentDidMount = () => {
-        this.position();
+        this.handlePosition();
+        this.getMilli();
     }
-    position = () => {
+    
+    handlePosition = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
-            console.log(lat);
             
             const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
             const fetchUrl = `http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`;
             fetch(proxyUrl + fetchUrl)
                 .then(response => response.json()) 
                 .then(data => {
-                    console.log(data.response[0].risetime);
                     this.setState ({
-                        passtime: data.response[0].risetime
+                        passtime: data.response[0].risetime * 1000
                     });
                 });
-                let newDate = new Date();
-                newDate.setTime(this.state.passtime*1000);
-        });
+                
+            });
+        }
+
+    getMilli = (milli) => {
+        let time = new Date(milli);
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        return hours + ":" + minutes;
     }
-        render(){
+            render(){
             return (
                 <div className="container">
-                    <h5>{`The International Space Station will be over me in ${this.state.passtime} minutes`}</h5>
+                    <h5>{`The next time the International Space Station will be over me will be at ${this.getMilli(this.state.passtime)} AM `}</h5>
                     <div className="fb-share-button" data-href="https://sofiarocha.github.io/iss" data-layout="box_count" data-size="small">
                         <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsofiarocha.github.io%2FChase-the-Space%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
                     </div>
