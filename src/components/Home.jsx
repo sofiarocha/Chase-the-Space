273ecb25d5bfdfcd4d26ff4cbd20c44197f70ce9
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-07 18:04:39 +0100

    add scroll to home page with react-ref

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TitleHomeContainer from './TitleHomeContainer';
 import Card from './Card';
 
@@ -7,19 +7,29 @@ const cards = [
     { title: "APOD", imageSrc: "https://res.cloudinary.com/dc4zctxhv/image/upload/v1557241446/Chase%20the%20Space/apod_square_taou7h.jpg", description: "The Astronomy Picture of the Day. Everyday NASA will provide us a new super cool picture from space." },
 ];
 
-const Home = () => {
-    return (
-        <div className="home-component">
-            <div className="section1">
-                <TitleHomeContainer />
-            </div>
-            <div className="section2">
-                <div className="row">
-                    {cards.map(card => <Card key={card.title} card={card} />)}
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+    scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop);
+
+    render() {
+        return (
+            <div className="home-component">
+                <div className="section1">
+                    <TitleHomeContainer handleScroll={this.scrollToMyRef} />
+                </div>
+                <div className="section2" ref={this.myRef}>
+                    <div className="row">
+                        {cards.map(card => <Card key={card.title} card={card} />)}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+
+    }
+}
 
 export default Home;
--- src/components/TitleHomeContainer.jsx
+++ src/components/TitleHomeContainer.jsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 
-const TitleHomeContainer = () => {
+const TitleHomeContainer = ({ handleScroll }) => {
     return (
         <Fragment>
             <h1>Chase the Space</h1>
-            <a href="#section2">
-                <i className="fas fa-chevron-down" />
-                <br />
-                    Scroll
-            </a>
+            <div className="scroller">
+                <button className="btn btn-link" onClick={handleScroll} type="button">
+                    <i className="fas fa-chevron-down" />
+                    <br />
+                        Scroll
+                </button>
+            </div>
         </Fragment>
     );
 };
