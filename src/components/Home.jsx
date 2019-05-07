9003a79ef110704a3c4bb5809db23c72ffa6b561
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-07 16:21:52 +0100

    put circle photos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import TitleHomeContainer from './TitleHomeContainer';
 import Card from './Card';
 
 const cards = [
-    { title: "ISS", imageSrc: "https://images.unsplash.com/photo-1446776877081-d282a0f896e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80", description: "The International Space Station is a large spacecraft in orbit around Earth. It serves as a home where crews of astronauts and cosmonauts live. The space station is also a unique science laboratory." },
-    { title: "APOD", imageSrc: "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80", description: "The Astronomy Picture of the Day. Everyday NASA will provide us a new super cool picture from space." },
+    { title: "ISS", imageSrc: "https://res.cloudinary.com/dc4zctxhv/image/upload/v1557241326/Chase%20the%20Space/iss_square_gjinjs.jpg", description: "The International Space Station is a large spacecraft in orbit around Earth. It serves as a home where crews of astronauts and cosmonauts live. The space station is also a unique science laboratory." },
+    { title: "APOD", imageSrc: "https://res.cloudinary.com/dc4zctxhv/image/upload/v1557241446/Chase%20the%20Space/apod_square_taou7h.jpg", description: "The Astronomy Picture of the Day. Everyday NASA will provide us a new super cool picture from space." },
 ];
 
 const Home = () => {
