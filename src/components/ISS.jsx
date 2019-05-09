aa0db0469d249df2a190728165977f11c79c4702
Isabella <isabellaafry@gmail.com>
2019-05-09 15:44:18 +0100

    fucking map

--- src/components/ISS.jsx
+++ src/components/ISS.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Leaflet from './Leaflet';
 import Sidebar from './Sidebar';
-import SelectedMap from './SelectedMap';
 import MapList from './MapList';
 
 const availableMaps = [
