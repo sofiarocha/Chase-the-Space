d882f0a88dcc4400c8126ec8ea10370732a7e137
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-09 16:20:23 +0100

    add style to apod page

--- src/components/TitleHomeContainer.jsx
+++ src/components/TitleHomeContainer.jsx
@@ -5,12 +5,10 @@ const TitleHomeContainer = ({ handleScroll }) => {
         <Fragment>
             <h1>Chase the Space</h1>
             <h5>“The universe seems neither benign nor hostile, merely indifferent.”</h5>
-                <p className="carl"> - Carl Sagan</p>
+            <p className="carl"> - Carl Sagan</p>
             <div className="scroller">
-                <button className="btn btn-link" onClick={handleScroll} type="button">
+                <button className="btn btn-link text-white" onClick={handleScroll} type="button">
                     <i className="fas fa-chevron-down" />
-                    <br />
-                        Scroll
                 </button>
             </div>
         </Fragment>
