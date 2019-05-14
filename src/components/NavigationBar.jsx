4908c9f593889df422f6f12ef23a5b2d71c94908
Isabella <isabellaafry@gmail.com>
2019-05-14 10:27:56 +0100

    menu collapses

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -12,26 +12,38 @@ import {
 class NavigationBar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            navExpanded: false
+        };
+    }
+
+    setNavExpanded = () => {
+        this.setState({ navExpanded: true });
+    }
+
+    closeNav = () => {
+        this.setState({ navExpanded: false });
     }
 
     render() {
+        const { navExpanded } = this.state;
         return (
             <div>
-                <Navbar collapseOnSelect expanded = {false} fixed="top" expand="lg" bg="dark" variant="dark">
+                <Navbar onToggle={this.setNavExpanded} expanded={navExpanded} collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
                     <Navbar.Brand>
                         <NavLink exact to="/" className="navbar-brand text-white">
                             <img src="https://www.pngkey.com/png/full/15-154688_black-hole-smash-ultimate-circle.png" width="50" height="50" className="d-inline-block" alt="" />
-ChaseTheSpace
+                            ChaseTheSpace
                         </NavLink>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto links">
+                        <Nav onClick={this.closeNav} className ="mr-auto links">
                             <NavLink exact to="/" className="nav-link text-white activeStyle">HOME</NavLink>
                             <NavLink to="/iss" className="nav-link text-white">ISS</NavLink>
                             <NavLink to="/apod" className="nav-link text-white">APOD</NavLink>
                         </Nav>
-                        <Nav className="aboutuslink">
+                        <Nav onClick={this.closeNav} className="aboutuslink">
                             <Link to="/about">About Us</Link>
                         </Nav>
                     </Navbar.Collapse>
